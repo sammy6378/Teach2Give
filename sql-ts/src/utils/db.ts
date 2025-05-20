1f7f10322dae0f5e7ce0af727dd845e5bbbbd728
sammy6378 <samuel202mwangi@gmail.com>
2025-05-20 12:13:57 +0300

    Refactor database connection to use connection pool and add executeQuery function

--- sql-ts/src/utils/db.ts
+++ sql-ts/src/utils/db.ts
@@ -23,15 +23,47 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
- export const pool = new Pool({
+ const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
     ssl: false, // Set true if you're connecting remotely and need SSL
+    max: 20,   // max no of clients in the pool 
+    idleTimeoutMillis: 30000, // close idle clients after 30 seconds
+    connectionTimeoutMillis: 2000 // return an error after 2 seconds if connection could not be established
   });
 
+  // emit error on idle clients
+  pool.on("error", (err, client) => {
+    console.error("Unexpected error on idle client", err);
+    process.exit(-1);
+  })
+
+  // function to excute queries
+  const executeQuery = async (query:string, params?: any[]) => {
+    const client = await pool.connect();
+    try {
+      const res = await client.query(query, params);
+       console.log('\nQuery:', query);
+        console.log('Results:', res.rows);
+        return res.rows;
+    } catch (err) {
+      console.error("Error executing query", err);
+      throw err;
+    } finally {
+      client.release();
+    }
+  }
+
+  // test connection
 export const connectDB = async () => {
- 
-  pool
-    .connect()
-    .then(() => console.log("✅ Connected to PostgreSQL"))
-    .catch((err) => console.error("❌ PostgreSQL connection error:", err));
+  try {
+    const client = await pool.connect();
+    console.log("✅ Connected to PostgreSQL");
+    client.release(); // release immediately after checking
+  } catch (err) {
+    console.error("❌ PostgreSQL connection error:", err);
+    process.exit(1);
+  }
 };
+
+
+export {pool, executeQuery};

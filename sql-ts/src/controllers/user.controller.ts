6f04e68e964a1cacb96dc57f4ea09d1358f0711f
sammy6378 <samuel202mwangi@gmail.com>
2025-05-20 15:41:11 +0300

    Add getOneUser function to retrieve user details by ID with validation

--- sql-ts/src/controllers/user.controller.ts
+++ sql-ts/src/controllers/user.controller.ts
@@ -65,3 +65,32 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+
+// get one user 
+export const getOneUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const userId = req.params.id;
+
+        if (!userId || isNaN(Number(userId))) {
+            res.status(400).json({ error: "Invalid user ID" });
+            return;
+        }
+
+        const result = await executeQuery(
+        `SELECT id, full_name, email, age, gender FROM users WHERE id = $1`,
+        [userId]
+        );
+
+
+        if (result.length === 0) {
+            res.status(404).json({ error: "User not found" });
+            return;
+        }
+
+        res.status(200).json({ success: true, user: result[0], message: "User found" });
+    } catch (error: any) {
+        console.error("Error fetching user:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};

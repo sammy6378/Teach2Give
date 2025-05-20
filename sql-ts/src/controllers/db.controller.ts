d9c7f6b2763a8444d81710b790955b51965de05a
sammy6378 <samuel202mwangi@gmail.com>
2025-05-20 12:13:10 +0300

    Refactor database controller to use executeQuery for fetching tables and table data

--- sql-ts/src/controllers/db.controller.ts
+++ sql-ts/src/controllers/db.controller.ts
@@ -1,15 +1,27 @@
-import { pool } from "../utils/db";
+import { pool,executeQuery } from "../utils/db";
 import { Request, Response } from "express";
 
 // Get all tables
 export const getAllTables = async (req:Request, res:Response) => {
   try {
-    const result = await pool.query(
+    const result = await executeQuery(
       "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
     );
-    res.status(200).json(result.rows);
+    res.status(200).json(result);
   } catch (error) {
     console.error("Error fetching tables:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+// get data from a table
+export const getTableData = async (req:Request, res:Response) => {
+  const tableName = req.params.tableName;
+  try {
+    const result = await executeQuery(`SELECT * FROM ${tableName}`);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error("Error fetching table data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}

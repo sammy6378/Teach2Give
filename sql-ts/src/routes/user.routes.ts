47919d545a4178bcd6317d4b4a374d466c4d9439
sammy6378 <samuel202mwangi@gmail.com>
2025-05-20 14:14:59 +0300

    Add user router with create-user endpoint

---
+++ sql-ts/src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+import { createUser } from "../controllers/user.controller";
+import { Router } from "express";
+
+
+export const userRouter = Router();
+
+// create a new user
+userRouter.post("/create-user", createUser);
